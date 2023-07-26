01ee6a9cf2e989e5112746d3c0a0699d8196c200
panth1823 <panthshah242@gmail.com>
2023-07-26 22:30:45 +0530

    initial commit

---
+++ G_AUTH/src/App.js
@@ -0,0 +1,16 @@
+// import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <div >
+      <Navbar />
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
